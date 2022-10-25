e1c3bc94e09e56647da3468ce27590fc6937ff76
namhyanghee <namhyanghee@218.38.137.27>
2022-10-25 22:42:39 +0900

    221025 - nhh - 로그인 수

--- src/main/java/login/controller/LoginController.java
+++ src/main/java/login/controller/LoginController.java
@@ -59,7 +59,7 @@ public class LoginController extends HttpServlet {
 			HttpSession session = req.getSession(true);
 			
 			//세션 정보 저장하기
-			session.setAttribute("login", true);		//로그인 상태
+//			session.setAttribute("login", true);		//로그인 상태
 			session.setAttribute("loginid", u_id);	//로그인한 아이디
 			
 			req.getRequestDispatcher("/WEB-INF/views/login/loginSuccess.jsp").forward(req, resp);
